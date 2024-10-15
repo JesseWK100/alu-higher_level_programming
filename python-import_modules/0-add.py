74c35ce98baac957ec1ba2b776ad0ebc06543526
JesseWK100 <j.walusansa@alustudent.com>
2024-10-16 00:16:29 +0200

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    # Import the add function from add_0.py
+    # Import the add function from add_0.py (only one occurrence of add_0)
     from add_0 import add
 
-    # Assign values to a and b
+    # Assign values to a and b in two different lines
     a = 1
     b = 2
 
-    # Print the result in the required format
+    # Use string formatting to display the result
     print("{} + {} = {}".format(a, b, add(a, b)))
