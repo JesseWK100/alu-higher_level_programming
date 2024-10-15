1006f0cdda28f79d0c1588e91bf1afd7cfda202c
JesseWK100 <j.walusansa@alustudent.com>
2024-10-16 00:25:34 +0200

    calculation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    # Import the functions from calculator_1.py
-    from calculator_1 import add, sub, mul, div
+    # Import all functions from calculator_1.py
+    import calculator_1
 
     # Define the variables a and b
     a = 10
     b = 5
 
     # Print the results using f-strings (no .format())
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+    print(f"{a} + {b} = {calculator_1.add(a, b)}")
+    print(f"{a} - {b} = {calculator_1.sub(a, b)}")
+    print(f"{a} * {b} = {calculator_1.mul(a, b)}")
+    print(f"{a} / {b} = {calculator_1.div(a, b)}")
