7937008cc9ad987fbde816e904aaf826a97832d6
JesseWK100 <j.walusansa@alustudent.com>
2024-10-15 22:32:52 +0200

    print alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 
-for i in range(97, 123):  # ASCII values for 'a' (97) to 'z' (122)
-    print(chr(i), end='')  # Print each character without a newline
+print(''.join(chr(i) for i in range(97, 123)), end='')
