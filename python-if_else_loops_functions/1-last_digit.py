1359d855fbc10b85a0fb28ea486e29c77945fdc9
JesseWK100 <j.walusansa@alustudent.com>
2024-10-15 22:27:49 +0200

    last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,9 @@ last_digit = abs(number) % 10  # Use abs to get the last digit
 # Print the results
 print(f"Last digit of {number} is", end=' ')
 
-# Check the value of last_digit and print appropriate message
+# Print the last digit with sign
 if number < 0:
-    print(f"-{last_digit}", end=' ')  # Ensure to print negative last digit
+    print(f"-{last_digit}", end=' ')  # Print negative last digit
 else:
     print(last_digit, end=' ')
 
