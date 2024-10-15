4710f9f8883588b3c78099f117c20326b17f580d
JesseWK100 <j.walusansa@alustudent.com>
2024-10-15 23:59:17 +0200

    print last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def print_last_digit(number):
     last_digit = abs(number) % 10  # Get the last digit
     print(last_digit, end='')  # Print the last digit without a newline
