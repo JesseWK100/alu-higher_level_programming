8749ba3f6517a6beb7457f44d4be1f1feceb9a74
JesseWK100 <j.walusansa@alustudent.com>
2024-10-15 23:47:51 +0200

    uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+def uppercase(str):
+    result = ""
+    for char in str:
+        # Check if the character is a lowercase letter
+        if ord(char) >= 97 and ord(char) <= 122:  # 'a' to 'z'
+            # Convert to uppercase by adjusting ASCII value
+            result += chr(ord(char) - 32)
+        else:
+            result += char  # Non-lowercase characters remain unchanged
+    print(result
