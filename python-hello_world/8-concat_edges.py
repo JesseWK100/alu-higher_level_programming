5acda861267e629e54c839d6ecf478f187d445c0
JesseWK100 <j.walusansa@alustudent.com>
2024-10-16 16:53:56 +0200

    concat edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 str = "object-oriented programming with Python"
-print(str[0:6] + str[7:18] + str[19:27] + str[-7:])
+print(str[0:6] + str[6] + str[7:18] + str[18] + str[19:27] + str[27:])
