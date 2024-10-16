f4c5939d8f524448d3622a39ac767631e6cdf393
JesseWK100 <j.walusansa@alustudent.com>
2024-10-16 16:42:06 +0200

    concat

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str1 = "Welcome to"
-str2 = "Battery Street!"
-str1 += " " + str2  # Concatenate str1 and str2 with a space
-print(str1)
+str2 = "Holberton School!"
+print(f"{str1} {str2}")
