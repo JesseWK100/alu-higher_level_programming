0ce17420c980d49012d35355787aabded5e4b15e
JesseWK100 <j.walusansa@alustudent.com>
2024-10-16 14:33:41 +0200

    delete

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     # Check if the index is valid
     if 0 <= idx < len(my_list):
-        # Create a new list without the item at the specified index
-        my_list = my_list[:idx] + my_list[idx + 1:]
+        # Use slicing to delete the element at the specified index
+        my_list[idx:idx + 1] = []  # This deletes the item at index idx
     
     return my_list
