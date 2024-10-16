f7886484f5538ebb27fa998d059cb39400f4f8fe
JesseWK100 <j.walusansa@alustudent.com>
2024-10-16 16:52:04 +0200

    concat edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[107:112] + str[:6])
+str = "object-oriented programming with Python"
+print(str[0:6] + str[7:18] + str[19:27] + str[-7:])
