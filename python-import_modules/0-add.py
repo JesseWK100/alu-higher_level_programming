fabcd0265a87a587fb182ad1ccd9af514ecad3e8
JesseWK100 <j.walusansa@alustudent.com>
2024-10-16 17:14:21 +0200

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    # Import the add function from add_0.py
-    from add_0 import add
+#!/usr/bin/python3
+from add_0 import add
 
-    # Assign values to a and b
-    a = 1
-    b = 2
+a = 1
+b = 2
 
-    # Use a single print statement with f-string formatting
-    print(f"{a} + {b} = {add(a, b)}")
+print("{} + {} = {}".format(a, b, add(a, b)))
