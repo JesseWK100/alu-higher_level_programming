d51a58027d9d726631661176fdc9448786a45112
JesseWK100 <j.walusansa@alustudent.com>
2024-10-30 14:08:32 +0200

    eight

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -59,14 +59,14 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return a string representation of the rectangle with print_symbol."""
+        """Return a string representation of the rectangle with print_symbol"""
         if self.__width == 0 or self.__height == 0:
             return ""
         symbol = str(self.print_symbol)
         return "\n".join([symbol * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
-        """Return a string representation to recreate a new instance using eval()."""
+        "Return a string representation to recreate a new instance usingeval()"
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
@@ -76,7 +76,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """Return the rectangle with the greater area, or rect_1 if areas are equal."""
+        "Return the rectangle with greater area, or rect_1 if areas are equal"
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
