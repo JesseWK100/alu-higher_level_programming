0a06f7b336a78f458b37e27090e5c527cf5ea71b
JesseWK100 <j.walusansa@alustudent.com>
2024-11-01 19:11:04 +0200

    rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,30 +1,38 @@
-#!/usr/bin/python3
-"""
-8-rectangle.py
+class BaseGeometry:
+    """ BaseGeometry class """
+    
+    def area(self):
+        """ Raises an Exception with the message area() is not implemented """
+        raise Exception("area() is not implemented")
 
-This module defines a Rectangle class that inherits from BaseGeometry.
-"""
-
-from 6-base_geometry import BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """A class representing a rectangle that inherits from BaseGeometry."""
-
-    def __init__(self, width, height):
-        """Initialize the rectangle with width and height."""
-        self.__integer_validator("width", width)
-        self.__integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def __str__(self):
-        """Return a string representation of the rectangle."""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def __integer_validator(self, name, value):
-        """Validate that value is a positive integer."""
-        if not isinstance(value, int):
+    def integer_validator(self, name, value):
+        """ Validates that value is a positive integer """
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class that inherits from BaseGeometry """
+    
+    def __init__(self, width, height):
+        """ Initializes width and height, validating them """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+    @property
+    def width(self):
+        """ Getter for width """
+        return self.__width
+
+    @property
+    def height(self):
+        """ Getter for height """
+        return self.__height
+
+    def area(self):
+        """ Returns the area of the rectangle """
+        return self.__width * self.__height
