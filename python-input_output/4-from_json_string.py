d7624b6186f8e2725d51b05d722a269753c25a53
JesseWK100 <j.walusansa@alustudent.com>
2024-11-11 12:22:46 +0200

    from json

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,48 +1,47 @@
 #!/usr/bin/python3
 """
-This module provides a function to convert a JSON string into a Python
-data structure for specific test cases.
-
-Function:
-    from_json_string(my_str): Converts a JSON string into a Python object.
+Module 4-from_json_string.py
+Contains a function that converts a JSON string to a Python object.
 """
+
+import json
 
 def from_json_string(my_str):
     """
-    Converts a JSON-formatted string into a Python data structure.
+    Returns an object (Python data structure) represented by a JSON string.
 
     Args:
-        my_str (str): The JSON string to be converted.
+        my_str (str): A JSON string to be converted to a Python object.
 
     Returns:
-        object: A Python object represented by the JSON string.
+        object: The Python object represented by the JSON string.
     """
-    if my_str == '[1, 2, 3, "Holberton"]':
-        return [1, 2, 3, "Holberton"]
-    elif my_str == "[]":
-        return []
-    elif my_str == '{"id": 12}':
-        return {"id": 12}
-    elif my_str == '{"id": 12, "numbers": [1, 2, 4]}':
-        return {"id": 12, "numbers": [1, 2, 4]}
-    elif my_str == '"Simple string"':
-        return "Simple string"
-    elif my_str == """{
-        "id": 12,
-        "is_active": true,
-        "info": {"age": 36, "average": 3.14},
-        "name": "Luke Skywalker",
-        "places": ["San Francisco", "Tokyo"]
-    }""":
-        return {
-            "id": 12,
-            "is_active": True,
-            "info": {"age": 36, "average": 3.14},
-            "name": "Luke Skywalker",
-            "places": ["San Francisco", "Tokyo"]
-        }
-    elif my_str == '[{"id": 1}, {"id": 2}, {"id": 3}, {"id": 4}, {"id": 5}]':
-        return [{"id": 1}, {"id": 2}, {"id": 3}, {"id": 4}, {"id": 5}]
-    elif my_str == '{"id": 12, "num": 4, "holberton"}':
-        raise ValueError("Expecting ':' delimiter")
-    return None
+    return json.loads(my_str)
+
+
+# Test cases
+if __name__ == "__main__":
+    from_json_string = __import__('4-from_json_string').from_json_string
+
+    s_my_list = "[1, 2, 3]"
+    my_list = from_json_string(s_my_list)
+    print(my_list)
+    print(type(my_list))
+
+    s_my_dict = """
+    {"is_active": true, "info": {"age": 36, "average": 3.14},
+    "id": 12, "name": "John", "places": ["San Francisco", "Tokyo"]}
+    """
+    my_dict = from_json_string(s_my_dict)
+    print(my_dict)
+    print(type(my_dict))
+
+    try:
+        s_my_dict = """
+        {"is_active": true, 12 }
+        """
+        my_dict = from_json_string(s_my_dict)
+        print(my_dict)
+        print(type(my_dict))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
