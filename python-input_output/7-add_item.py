2e794ad1bbe3c561479e13a70a778e7cd3e53e59
JesseWK100 <j.walusansa@alustudent.com>
2024-11-11 14:03:19 +0200

    add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,49 +1,25 @@
 #!/usr/bin/python3
 """
-Module 7-add_item.py
-Adds all arguments to a Python list and saves them to a JSON file.
+A script that adds all command line arguments to a Python list, and then
+saves them to a file as a JSON representation.
 """
 
 import sys
-import json
+from os import path
 
-def save_to_json_file(my_obj, filename):
-    """
-    Writes an object to a text file using a JSON representation.
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    Args:
-        my_obj (object): The object to be written to the file.
-        filename (str): The name of the file to write to.
-    """
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
+filename = "add_item.json"
 
+# Load existing data from file, or start with an empty list if the file doesn't exist
+if path.exists(filename):
+    data = load_from_json_file(filename)
+else:
+    data = []
 
-def load_from_json_file(filename):
-    """
-    Creates an object from a JSON file.
+# Add command line arguments to the list
+data.extend(sys.argv[1:])
 
-    Args:
-        filename (str): The name of the file to read from.
-    
-    Returns:
-        object: The Python object created from the JSON file.
-    """
-    with open(filename, 'r') as file:
-        return json.load(file)
-
-
-if __name__ == "__main__":
-    filename = "add_item.json"
-
-    # Try to load existing data from the file
-    try:
-        items = load_from_json_file(filename)
-    except FileNotFoundError:
-        items = []
-
-    # Add all arguments to the list
-    items.extend(sys.argv[1:])
-
-    # Save the updated list back to the file
-    save_to_json_file(items, filename)
+# Save updated list to file
+save_to_json_file(data, filename)
