8d09f0a767ab925d386f1c3d778511c18f15b919
JesseWK100 <j.walusansa@alustudent.com>
2024-11-12 13:06:40 +0200

    privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,13 +1,47 @@
--- This script creates the users 'user_0d_1' and 'user_0d_2', grants privileges,
--- and lists all privileges of the MySQL users 'user_0d_1' and 'user_0d_2' on the server
+-- This script lists all privileges of the MySQL users 'user_0d_1' and 'user_0d_2' on the server
 -- Save this script in a file named '0-privileges.sql'
 
-CREATE USER 'user_0d_1'@'localhost';
-CREATE USER 'user_0d_2'@'localhost';
+-- Check if user_0d_1 exists
+SELECT 'Checking if user_0d_1 exists' AS Message;
+SELECT user FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
+-- If user_0d_1 does not exist, create the user
+DROP PROCEDURE IF EXISTS create_user_0d_1;
+DELIMITER //
+CREATE PROCEDURE create_user_0d_1()
+BEGIN
+    DECLARE user_count INT;
+    SELECT COUNT(*) INTO user_count FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost';
+    IF user_count = 0 THEN
+        CREATE USER 'user_0d_1'@'localhost';
+        GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+    END IF;
+END //
+DELIMITER ;
+CALL create_user_0d_1();
+DROP PROCEDURE create_user_0d_1;
 
+-- Check if user_0d_2 exists
+SELECT 'Checking if user_0d_2 exists' AS Message;
+SELECT user FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost';
+
+-- If user_0d_2 does not exist, create the user
+DROP PROCEDURE IF EXISTS create_user_0d_2;
+DELIMITER //
+CREATE PROCEDURE create_user_0d_2()
+BEGIN
+    DECLARE user_count INT;
+    SELECT COUNT(*) INTO user_count FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost';
+    IF user_count = 0 THEN
+        CREATE USER 'user_0d_2'@'localhost';
+        GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
+    END IF;
+END //
+DELIMITER ;
+CALL create_user_0d_2();
+DROP PROCEDURE create_user_0d_2;
+
+-- Show grants for users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
