d6e161e1383a8d0af3ffee91fc2cdfab5c119abe
JesseWK100 <j.walusansa@alustudent.com>
2024-11-15 00:10:47 +0200

    my genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,10 @@
--- Lists all genres of the show 'Dexter' from the hbtn_0d_tvshows database.
+-- Select the genre names for the show titled 'Dexter'
 SELECT genres.name
+-- Join the tv_shows and tv_show_genres tables
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 JOIN genres ON tv_show_genres.genre_id = genres.id
+-- Filter the results to include only the show titled 'Dexter'
 WHERE tv_shows.title = 'Dexter'
+-- Sort the results in ascending order by the genre name
 ORDER BY genres.name ASC;
