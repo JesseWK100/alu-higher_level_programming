ca9f7b6653ff926cedb846a1980eaea3ffcd8038
JesseWK100 <j.walusansa@alustudent.com>
2024-11-15 00:00:31 +0200

    count shows

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,11 +1,11 @@
--- Selects the genre name and counts the number of shows linked to each genre.
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
--- Joins the tv_show_genres table with genres to get genre names for each genre_id.
+-- Select the genre name and the count of shows linked to each genre
+SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+-- Join the tv_show_genres table with the genres table
 FROM tv_show_genres
 JOIN genres ON tv_show_genres.genre_id = genres.id
--- Groups the results by genre name to get a count for each genre.
+-- Group the results by genre name
 GROUP BY genres.name
--- Ensures only genres with shows linked are displayed.
-HAVING number_of_shows > 0
--- Orders the results in descending order by the number of shows linked.
+-- Filter out genres with no linked shows
+HAVING COUNT(tv_show_genres.tv_show_id) > 0
+-- Sort the results in descending order by the number of shows linked
 ORDER BY number_of_shows DESC;
