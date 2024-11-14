5b59ad402040e32a42a1f01e4ed77a037a97ada4
JesseWK100 <j.walusansa@alustudent.com>
2024-11-15 00:41:14 +0200

    my genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,9 @@
--- Select the genre names for the show titled 'Dexter'
-SELECT genres.name
--- Join the tv_shows and tv_show_genres tables
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN genres ON tv_show_genres.genre_id = genres.id
--- Filter the results to include only the show titled 'Dexter'
-WHERE tv_shows.title = 'Dexter'
--- Sort the results in ascending order by the genre name
-ORDER BY genres.name ASC;
+-- RECODES: 1
+SELECT g.`name`
+ FROM `tv_genres` AS g
+	INNER JOIN `tv_show_genres` AS s
+	ON g.`id` = s.`genre_id`
+	INNER JOIN `tv_shows` AS t
+	ON t.`id` = s.`show_id`
+	WHERE t.`title` = "Dexter"
+ORDER BY g.`name`
