856cad50587fed6ca2feff7ca852a4e3bd3b8a9d
JesseWK100 <j.walusansa@alustudent.com>
2024-11-14 23:45:49 +0200

    genre

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,7 @@
+-- Select the title of the TV show and its associated genre ID
 SELECT tv_shows.title, tv_show_genres.genre_id
+-- Join the tv_shows and tv_show_genres tables on the tv_show_id field
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+-- Order the results in ascending order by tv show title and genre ID
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
