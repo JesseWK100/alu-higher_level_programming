63b92794ca0cc79a18996285388563a3538df688
JesseWK100 <j.walusansa@alustudent.com>
2024-11-17 20:30:02 +0200

    status

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetches https://alu-intranet.hbtn.io/status using urllib"""
+import urllib.request
+
+url = 'https://alu-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print("Body response:")
+print(f"\t- type: {type(body)}")
+print(f"\t- content: {body}")
+print(f"\t- utf8 content: {body.decode('utf-8')}")
