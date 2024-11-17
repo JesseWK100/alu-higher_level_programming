c4f7e029379e5ef3142a1807c4b310365955ddbe
JesseWK100 <j.walusansa@alustudent.com>
2024-11-17 19:40:58 +0200

    body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Script that takes a URL, sends a request, and displays the size of the response body in bytes
-
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-curl -s -o /dev/null -w "%{size_download}\n" "$1"
+# Send a request to the provided URL and display the size of the response body
+curl -s -o /dev/null -w "%{size_download}\n" "$"
