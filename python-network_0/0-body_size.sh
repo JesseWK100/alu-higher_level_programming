adabd53560c9aa521661c124be7cbc75c61fa88c
JesseWK100 <j.walusansa@alustudent.com>
2024-11-17 19:43:43 +0200

    body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a request to the provided URL and display the size of the response body
-curl -s -o /dev/null -w "%{size_download}\n" "$"
+# Bash script to get the size of the body of the response
+curl -s "$1" | wc -c
