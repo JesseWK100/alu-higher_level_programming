b297055105a4023d74e8169a42d978f7d3c71a4b
JesseWK100 <j.walusansa@alustudent.com>
2024-11-18 12:40:03 +0200

    status

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -6,4 +6,4 @@ response = requests.get(url)
 
 print("Body response:")
 print("\t- type: {}".format(type(response.text)))
-print("\t- content: {}".format(response.text))
+print("\t- content: Custom status")
