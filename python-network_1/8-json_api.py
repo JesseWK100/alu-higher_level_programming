fd161ef3c9501376eee46d3bfe7d13cee227eac8
JesseWK100 <j.walusansa@alustudent.com>
2024-11-18 13:12:46 +0200

    json

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-This script takes in a letter and sends a POST request to 
+This script takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter.
 
 The letter must be sent in the variable q. If no argument is given, set q="".
-If the response body is properly JSON formatted and not empty, display the id and name.
+If response body is properly JSON formatted and not empty, display id and name
 Otherwise:
     - Display "Not a valid JSON" if the JSON is invalid
     - Display "No result" if the JSON is empty
@@ -19,13 +19,13 @@ import sys
 if __name__ == "__main__":
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
     payload = {'q': letter}
-    
+   
     response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
 
     try:
         json_response = response.json()
         if json_response:
-            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+            print("[{}] {}".format(json_response.get("id"), json_response.get("name")))
         else:
             print("No result")
     except ValueError:
