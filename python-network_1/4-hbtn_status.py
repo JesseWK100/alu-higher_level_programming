6173f686798b3da56d57c3cdab9847dedd3cb897
JesseWK100 <j.walusansa@alustudent.com>
2024-11-18 14:55:09 +0200

    status

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """
-This script fetches the content of the URL https://alu-intranet.hbtn.io/status
-using the `requests` package and displays the response in a specific format.
+This script fetches https://alu-intranet.hbtn.io/status
+and displays the response body with information about
+its type and content.
 
-Output Example:
-    Body response:
-        - type: <class 'str'>
-        - content: Custom status
+Usage:
+    ./4-hbtn_status.py
 """
 
 import requests
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    url = 'https://alu-intranet.hbtn.io/status'
     response = requests.get(url)
-
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
-    print("\t- content: Custom status")
+    print("\t- content: {}".format(response.text))
