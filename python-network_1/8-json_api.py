833e2fbda8a92e4572e9765e801d67aaf10eb4b2
JesseWK100 <j.walusansa@alustudent.com>
2024-11-18 13:24:48 +0200

    json

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -19,14 +19,14 @@ import sys
 if __name__ == "__main__":
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
     payload = {'q': letter}
- 
+
     response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
 
     try:
         json_response = response.json()
         if json_response:
             print("[{}] {}".format(json_response.get("id"),
-                    json_response.get("name")))
+                            json_response.get("name")))
         else:
             print("No result")
     except ValueError:
