4011238cdcfeafcf5798d83fb2983a7ca749fc44
JesseWK100 <j.walusansa@alustudent.com>
2024-11-18 14:50:10 +0200

    statis

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script fetches https://alu-intranet.hbtn.io/status
+This script fetches http://0.0.0.0:5050/status
 and displays the response body with information about
 its type, content, and utf-8 decoded content.
 
@@ -11,13 +11,10 @@ Usage:
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    url = "http://0.0.0.0:5050/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
-        # Concatenating 'OK' and 'Custom status' for demonstration
-        combined_content = body + b' Custom status'
-        utf8_combined_content = combined_content.decode('utf-8')
         print("Body response:")
-        print("\t- type: {}".format(type(combined_content)))
-        print("\t- content: {}".format(combined_content))
-        print("\t- utf8 content: {}".format(utf8_combined_content))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
