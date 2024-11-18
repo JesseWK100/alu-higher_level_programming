35d43d0f2424482277cf1044e889784af2098bd1
JesseWK100 <j.walusansa@alustudent.com>
2024-11-18 14:39:45 +0200

    github

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -17,10 +17,10 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     token = sys.argv[2]
-    
+
     url = "https://api.github.com/user"
     response = requests.get(url, auth=(username, token))
-    
+
     try:
         print(response.json().get("id"))
     except ValueError:
