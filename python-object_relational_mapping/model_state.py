c39d6a8f0a8153c099311afbbc8d7dc3b1b77dda
JesseWK100 <j.walusansa@alustudent.com>
2024-11-20 02:58:00 +0200

    model states

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """
     State class that inherits from Base.
@@ -19,10 +20,11 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
+
 # Only execute this when run as a script
 if __name__ == "__main__":
     import sys
-    # Create an engine that connects to the MySQL server 
+    # Create an engine that connects to the MySQL server
     # running on localhost at port 3306.
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
