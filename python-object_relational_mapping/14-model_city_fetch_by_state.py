28b0b35471f1cf2bb9ff919b42d4263430f1e1f6
JesseWK100 <j.walusansa@alustudent.com>
2024-11-20 03:44:41 +0200

    state

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
 """
-This script prints all City objects from the database hbtn_0e_14_usa.
-It takes three arguments: mysql username, mysql password, and database name.
+Prints all City objects from the database hbtn_0e_14_usa using SQLAlchemy.
 """
 
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
-import sys
 
 if __name__ == "__main__":
-    # Create an engine that connects to the MySQL server running on localhost
-    # at port 3306
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-
-    # Create all tables in the database (if they don't exist)
+    # Create engine and connect to the MySQL database
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}',
+        pool_pre_ping=True
+    )
     Base.metadata.create_all(engine)
 
-    # Create a configured "Session" class
+    # Create a new session
     Session = sessionmaker(bind=engine)
-
-    # Create a Session
     session = Session()
 
-    # Query all City objects and join with State objects, sorted by cities.id
-    cities = session.query(City).join(State).order_by(City.id).all()
+    # Query all City objects sorted by cities.id
+    results = session.query(City, State).join(State).order_by(City.id).all()
 
     # Print the results
-    for city in cities:
-        print(f"{city.state.name}: ({city.id}) {city.name}")
+    for city, state in results:
+        print(f"{state.name}: ({city.id}) {city.name}")
 
     # Close the session
     session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """
-This module contains the class definition of a City.
+Contains the class definition of a City and an instance of Base using
+SQLAlchemy.
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
 class City(Base):
     """
-    City class that inherits from Base.
-    Links to the MySQL table cities.
+    City class:
+    - Inherits from Base.
+    - Links to the MySQL table `cities`.
     """
     __tablename__ = 'cities'
 
