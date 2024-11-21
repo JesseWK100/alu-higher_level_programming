81bd5b51ec9ea06e9bbf873876fcca55f3fabac2
JesseWK100 <j.walusansa@alustudent.com>
2024-11-21 19:40:00 +0200

    update changes

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,17 +1,18 @@
-import unittest
-from models.base import Base
+#!/usr/bin/python3
+"""Base class module."""
 
+class Base:
+    """Represents the base of all other classes in this project."""
+    __nb_objects = 0
 
-class TestBase(unittest.TestCase):
-    def test_id_assignment(self):
-        b1 = Base()
-        b2 = Base()
-        b3 = Base(12)
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 12)
-
-    def test_to_json_string(self):
-        data = [{"id": 1, "name": "Rectangle"}]
-        json_data = Base.to_json_string(data)
-        self.assertEqual(json_data, '[{"id": 1, "name": "Rectangle"}]')
+    def __init__(self, id=None):
+        """Initializes a new instance of Base.
+        
+        Args:
+            id (int): The id of the instance. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
